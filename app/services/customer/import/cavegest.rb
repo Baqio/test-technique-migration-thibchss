a337fddb62c291541b08c36490cb22636c888b66
Thibault C. <thibault.chassine@gmail.com>
2026-09-16 18:44:29 +0200

    Add creation date to customer import

--- app/services/customer/import/cavegest.rb
+++ app/services/customer/import/cavegest.rb
@@ -27,6 +27,7 @@ class Customer::Import::Cavegest < Importer::Base
         price_grid_code: N.text(row[indexes.price_grid_code]),
         vat_number: N.text(row[indexes.vat_number]),
         excise_number: N.text(row[indexes.excise_number]),
+        creation_date: N.text(row[indexes.creation_date])&.to_date,
         active: N.text(row[indexes.active]).to_i.zero?,
         **Customer::Import::Cavegest::Address.new(row, indexes).call
       )
