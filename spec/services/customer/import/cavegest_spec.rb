d1299e61605c550cae099fd4a756401039e3f529
Thibault C. <thibault.chassine@gmail.com>
2026-09-17 20:53:15 +0200

    Add specs for new logic and check errors and warnings

--- spec/services/customer/import/cavegest_spec.rb
+++ spec/services/customer/import/cavegest_spec.rb
@@ -1,7 +1,12 @@
 require "spec_helper"
 
 RSpec.describe Customer::Import::Cavegest do
-  before(:all) { described_class.new(data_path("export_clients_cavegest.xlsx")).call }
+  before(:all) do
+    @report = MigrationReport.new
+    described_class.new(data_path("export_clients_cavegest.xlsx"), report: @report).call
+  end
+
+  let(:report) { @report }
 
   it "keeps the leading zero of postal codes" do
     expect(Customer.find_by(reference: "T00022").zip).to eq("01000")
@@ -40,5 +45,62 @@ RSpec.describe Customer::Import::Cavegest do
     expect(deutsch_customer.phone).to eq("+49556174676")
     expect(belgian_customer.phone).to eq("+32272111757")
     expect(french_customer.phone).to eq("+33466824417")
+  end
+
+  shared_examples 'report error' do
+    it 'records an error in the report' do
+      error = report.errors.find { |e| e.locator == reference }
+
+      expect(error).not_to be_nil
+      expect(error.message).to eq(message)
+    end
+  end
+
+  context 'when there is no first_name, last_name or company_name' do
+    let(:reference) { 'T00078' }
+    let(:message) { 'Raison sociale, nom et prénom manquants' }
+
+    it 'does not persist the customer' do
+      customer = Customer.find_by(reference: reference)
+  
+      expect(customer).to be_nil
+    end
+
+    include_examples 'report error'
+  end
+
+  context 'when there is a duplicated reference' do
+    let(:reference) { 'T00101' }
+    let(:message) { 'La référence est déjà existante' }
+
+    it 'does not persist the customer' do
+      customer = Customer.where(reference: reference)
+  
+      expect(customer.size).to eq(1)
+    end
+
+    include_examples 'report error'
+  end
+
+  it 'tracks the created and not created customers' do
+    expect(report.counters).to include(
+      {
+        customer_created: 4991,
+        customer_not_created: 6
+      }
+    )
+  end
+
+  it 'collects the proper warnings' do
+    warnings_tallied = report.warnings.map(&:message).tally
+
+    expect(warnings_tallied).to include(
+      {
+        "Pays pour l'adresse de facturation ou d'expédition manquant ou non reconnu" => 990,
+        'Client est marqué comme inutilisable/inactif' => 340,
+        'Aucune information de contact' => 17,
+        "Aucune information de contact pour la facturation ou l'expédition"=>2
+      }
+    )
   end
 end
